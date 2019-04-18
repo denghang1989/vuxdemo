50b5be7e8923b732ce449e87762738e01337af02
denghang1989 <denghang1989@163.com>
2019-04-19 07:44:01 +0800

    修改3

--- src/page/PatientDetail.vue
+++ src/page/PatientDetail.vue
@@ -1,15 +1,66 @@
 <template>
   <div class="detail">
-    hehe
+    <swiper auto height="180px">
+      <swiper-item class="black"><h2 class="title fadeInUp animated">它无孔不入</h2></swiper-item>
+      <swiper-item class="black"><h2 class="title fadeInUp animated">你无处可藏</h2></swiper-item>
+      <swiper-item class="black"><h2 class="title fadeInUp animated">不是它可恶</h2></swiper-item>
+      <swiper-item class="black"><h2 class="title fadeInUp animated">而是它不懂你</h2></swiper-item>
+      <swiper-item class="black"><h2 class="title fadeInUp animated">我们试图</h2></swiper-item>
+      <swiper-item class="black"><h2 class="title fadeInUp animated">做些改变</h2></swiper-item>
+    </swiper>
+    <div class="content">
+      <box gap="10px 10px">
+        <x-button :link="link">确定11</x-button>
+        <x-button :link="link">确定22</x-button>
+        <x-button :link="link">确定33</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定</x-button>
+        <x-button :link="link">确定55</x-button>
+        <x-button :link="link">确定66</x-button>
+        <x-button :link="link">确定77</x-button>
+      </box>
+    </div>
   </div>
 </template>
 
 <script>
+  import {SwiperItem, Swiper, Box, XButton} from 'vux';
+  
   export default {
     name: "PatientDetail",
+    components: {SwiperItem, Swiper, Box, XButton},
     data() {
       return {
-        patient: {}
+        patient: {},
+        link:"/listPage"
       }
     },
     methods: {
